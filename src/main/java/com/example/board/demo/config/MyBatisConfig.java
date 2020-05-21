aa345e60b433ed24653741b598ab56b2646052c8
ByeonGooHoon <dksekfldks65@naver.com>
2020-05-21 17:59:11 +0900

    Initial commit

---
+++ src/main/java/com/example/board/demo/config/MyBatisConfig.java
@@ -0,0 +1,36 @@
+package com.example.board.demo.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MyBatisConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory (DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setTypeAliasesPackage("com.board.example.demo.dto");
+        sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml"));
+        sqlSessionFactory.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/*.xml"));
+
+        return sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession (SqlSessionFactory sqlSessionFactory) {
+
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
